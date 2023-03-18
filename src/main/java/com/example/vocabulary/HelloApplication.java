47bda62b957df9c985d7dc86fc800b510dc4ab57
NikolaiProhorenko <kolman2008@mail.ru>
2023-03-18 19:37:28 +0300

    DzLesson_11

--- src/main/java/com/example/vocabulary/HelloApplication.java
+++ src/main/java/com/example/vocabulary/HelloApplication.java
@@ -10,7 +10,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("vocabulary.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 348, 326);
+        Scene scene = new Scene(fxmlLoader.load(), 379, 348);
         stage.setTitle("My first Vocabulary by Nick");
         stage.setScene(scene);
         stage.show();
